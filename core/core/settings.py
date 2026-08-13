009eaead1bcb6032e939f843de23beaaf03e205e
amirtaherii81 <amir.taheri81ir@gmail.com>
2026-08-13 17:13:38 +0330

    static and media setup
    setting: STATICFILES_DIRS and ROOT
    urls: servin static and media for development

--- core/core/settings.py
+++ core/core/settings.py
@@ -117,7 +117,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR / "static"
+
+STATIC_URL = "/meida/"
+STATIC_ROOT = BASE_DIR / "media"
+
+STATICFILES_DIRS = [
+    BASE_DIR / "staticfiles",
+]
 
 # user manager config
 AUTH_USER_MODEL = "accounts.User"
