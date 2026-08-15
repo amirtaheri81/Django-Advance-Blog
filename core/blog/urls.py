5935daea32ed49926a650b0db91669d3b13c03c6
amirtaherii81 <amir.taheri81ir@gmail.com>
2026-08-15 18:24:59 +0330

    added update, delete and create post

--- core/blog/urls.py
+++ core/blog/urls.py
@@ -4,6 +4,9 @@ from . import views
 app_name ='blog'
 
 urlpatterns = [
-    path("/", views.PostListView.as_view(), name='post_list'),
-    path("post/<int:pk>/", views.PostDetailView.as_view(), name='post_datail'),
+    path('', views.PostListView.as_view(), name='post_list'),
+    path('post/<int:pk>/', views.PostDetailView.as_view(), name='post_datail'),
+    path('post/add/', views.PostCreateView.as_view(), name='post_add'),
+    path('post/<int:pk>/edit/', views.PostUpdateView.as_view(), name='post_edit'),
+    path('post/<int:pk>/del/', views.PostDeleteView.as_view(), name='post_delete')
 ]
--- core/blog/views.py
+++ core/blog/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, DeleteView, FormView, UpdateView
 from .models import Post
+from .forms import PostForm
+from django.urls import reverse
+from django.http import HttpResponseRedirect
 # Create your views here.
 
 
@@ -13,3 +17,22 @@ class PostListView(ListView):
     
 class PostDetailView(DetailView):
     model = Post
+    
+    
+class PostCreateView(CreateView):
+    model = Post
+    form_class = PostForm
+    success_url = '/blog/post/add/'
+      
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super(PostCreateView, self).form_valid(form)
+    
+class PostUpdateView(UpdateView):
+    model = Post
+    form_class = PostForm
+    
+  
+class PostDeleteView(DeleteView):
+    model = Post
+    success_url = '/blog/'
