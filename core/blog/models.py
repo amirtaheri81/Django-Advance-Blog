687ec85f99acb98095bd3a7a4577ac1b92d80bb6
amirtaherii81 <amir.taheri81ir@gmail.com>
2026-08-15 18:55:43 +0330

    change field author in post

--- core/blog/models.py
+++ core/blog/models.py
@@ -1,12 +1,10 @@
 from django.db import models
-from django.contrib.auth import get_user_model
-
+# from django.contrib.auth import get_user_model
 
 # getting user model object
-user = get_user_model()
 
 class Post(models.Model):
-    author = models.ForeignKey(user, on_delete=models.CASCADE)
+    author = models.ForeignKey('accounts.Profile', on_delete=models.CASCADE)
     image = models.ImageField(null=True, blank=True)
     title = models.CharField(max_length=255)
     content = models.TextField()
